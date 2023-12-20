4acb6096ec4afbb14de3cff78516ae45794b783a
Kaiqueav <kaiquecerq66@outlook.com>
2023-12-19 23:25:44 -0300

    criação do componente work

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,16 @@
     import Header from './components/Header.vue'
     import Banner from './components/Banner.vue'
     import About from './components/About.vue'
+    import Work from './components/Work.vue'
+    
 </script>
 
 <template>
     <div>
          <Header logo = "/img/Header.Logo.png" />
          <Banner check = "/img/check-check.png" />
-         <About heart = "/img/heart-handshake.png" world="/img/globe-2.png" smile="/img/smile-plus.png" users="/img/users.png"      />
+         <About heart = "/img/heart-handshake.png" world="/img/globe-2.png" smile="/img/smile-plus.png" users="/img/users.png"/>
+         <Work />
     </div>
 
 </template>
---
+++ src/components/Work.vue
@@ -0,0 +1,41 @@
+<script setup>
+
+
+</script>
+
+
+<template>
+ <div class="p-2.5 mt-4">
+    <div class="flex flex-row">
+        <h1 class="text-[#760EC8] text-8xl font-bold ml-0"> 1</h1>
+        <h2 class="text-white font-bold text-3xl mt-7 ml-2.5"> Cadastre-se na Plataforma</h2>
+    </div>
+        <p class="text-[#b6b2b2] p-2.5">
+            Registre-se na plataforma preenchendo o formulário com suas
+             informações e escolhendo seu perfil (programador, designer ou outro).
+              É rápido e simples! 
+        </p>
+        <div class="flex flex-row">
+        <h1 class="text-[#760EC8] text-8xl font-bold ml-1.5"> 2</h1>
+        <h2 class="text-white font-bold text-3xl mt-7  ml-2.5"> Explore os Desafios </h2>
+    </div>
+        <p class="text-[#b6b2b2] p-2.5">
+            Navegue por desafios emocionantes! 
+            Encontre projetos alinhados com suas habilidades.
+             Filtrar por categoria e dificuldade para as melhores opções. 
+        </p>
+        <div class="flex flex-row">
+        <h1 class="text-[#760EC8] text-8xl font-bold ml-1.5"> 3</h1>
+        <h2 class="text-white font-bold text-3xl mt-7  ml-2.5"> Enfrente os Desafios e Cresça </h2>
+    </div>
+        <p class="text-[#b6b2b2] p-2.5">
+            Escolha um desafio que atraia e coloque suas habilidades à prova.
+             Trabalhe em equipe ou individualmente.
+              Receba feedback valioso e construa sua reputação!
+        </p>
+ </div>
+</template>7
+<style scoped>
+
+
+</style>
